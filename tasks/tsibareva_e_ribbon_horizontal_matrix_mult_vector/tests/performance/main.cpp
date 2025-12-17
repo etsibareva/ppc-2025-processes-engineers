451a337eb38b38357abeac871769c37256c45946
etsibareva <etsibareva.university@gmail.com>
2025-12-17 17:29:50 +0300

    CommitX: 8x8 and test coverage corrected

--- tasks/tsibareva_e_ribbon_horizontal_matrix_mult_vector/tests/performance/main.cpp
+++ tasks/tsibareva_e_ribbon_horizontal_matrix_mult_vector/tests/performance/main.cpp
@@ -12,8 +12,8 @@
 namespace tsibareva_e_ribbon_horizontal_matrix_mult_vector {
 
 class TsibarevaERunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kMatrixRows_ = 10000;
-  const int kMatrixCols_ = 10000;
+  const int kMatrixRows_ = 8000;
+  const int kMatrixCols_ = 8000;
   InType input_data_;
   OutType expected_output_;
 
